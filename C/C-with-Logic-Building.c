0f2b566d4dc961641537e3b5835168274d2f904e
Paramveer Singh <negiparamveer7@gmail.com>
2025-11-03 17:36:42 +0530

    Day-1 Questions

---
+++ C/C-with-Logic-Building.c
@@ -0,0 +1,57 @@
+// gcc -std=c11 -o a.out C-with-Logic-Building.c && a.out
+// Question 1 ********Assignment-1_Question-1 ***************
+// Write a program to print "Hello, Students" on the screen.
+/*
+#include <stdio.h>
+int main()
+{
+  printf("Hello, Student!\n");
+  return 0;
+}
+*/
+
+// Question 2 ********Assignment-1_Question-2 ***************
+// Write a program to print Hello in the first line and Students in the second line
+/*
+#include <stdio.h>
+int main()
+{
+  printf("Hello,\n");
+  printf("Student!\n");
+  // printf("Hello,\nStudent!\n");
+  return 0;
+}
+*/
+
+// Question 3*******Assignment-1_Question-3 ***************
+/*
+// Write a program to print "MySirG" on the screen. (Remember to use double quotes)
+#include <stdio.h>
+int main()
+{
+  printf("\"MySirG\"");
+  return 0;
+}
+*/
+
+// Question 4 *******Assignment-1_Question-4 ***************
+/*
+// Write a program to print \n on the screen
+#include <stdio.h>
+int main()
+{
+  printf("\\n");
+  return 0;
+}
+*/
+
+// Question 5 *******Assignment-1_Question-5 ***************
+// Write a program to print \r on the screen
+/*
+#include <stdio.h>
+int main()
+{
+  printf("\\r");
+  return 0;
+}
+// */
